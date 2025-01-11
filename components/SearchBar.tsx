e82a55e4cbbe9bd17fda5ccd91b52407217f1caa
Truong Thai <52000714@student.tdtu.edu.vn>
2025-01-12 04:12:16 +0700

    Done Search Function

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,45 +1,86 @@
 "use client";
 
-import { ReactNode, useState } from "react";
-import SearchManufacture from "./SearchManufacture";
 import Image from "next/image";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import SearchManufacture from "./SearchManufacture";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState("");
-  const handleSearch = () => {};
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
+  const router = useRouter();
 
-  const SearchButton = ({ otherClass } : any) => {
-    return (
-      <button type="button" className={`-ml-3 z-10 ${otherClass}`}>
-        <Image
-          alt="magnifying"
-          src="/magnifying-glass.svg"
-          width={40}
-          height={40}
-          className="object-contain"
-        />
-      </button>
-    )
-  }
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(manufacturer, model)
+
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <SearchManufacture
           manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          setManufacturer={setManuFacturer}
         />
-        <SearchButton />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
       <div className="searchbar__item">
         <Image
-          alt="search"
           src="/model-icon.png"
           width={25}
           height={25}
           className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
         />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan..."
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
--- components/SearchManufacture.tsx
+++ components/SearchManufacture.tsx
@@ -1,30 +1,34 @@
 "use client";
 import { manufacturers } from "@/constants";
 import { SearchManufactureProps } from "@/type";
-import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Transition } from "@headlessui/react";
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Transition,
+} from "@headlessui/react";
 import Image from "next/image";
 import React, { Fragment, useState } from "react";
-
 
 const SearchManufacture = ({
   manufacturer,
   setManufacturer,
 }: SearchManufactureProps) => {
   const [query, setQery] = useState("");
-  const filterManufacturers = 
-    query === "" 
-      ? manufacturers 
-      : manufacturers.filter((item) => (
-        item.toLocaleLowerCase()
-        .replace(/\s+/g, "")
-        .includes(
-          query.toLocaleLowerCase()
-          .replace(/\s+/g, "")
-        )
-      ))
+  const filterManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLocaleLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLocaleLowerCase().replace(/\s+/g, ""))
+        );
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <ComboboxButton className="absolute top-[14px]">
             <Image
@@ -46,24 +50,40 @@ const SearchManufacture = ({
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            afterLeave={() => setQery('')}
+            afterLeave={() => setQery("")}
           >
             <ComboboxOptions>
-              {filterManufacturers.length === 0 && 
-                query !== "" ? (
-                <ComboboxOption 
+              {filterManufacturers.length === 0 && query !== "" ? (
+                <ComboboxOption
                   value={query}
-                  className='search-manufacturer__option'
+                  className="search-manufacturer__option"
                 >
                   Create "{query}"
                 </ComboboxOption>
-              ): (
+              ) : (
                 filterManufacturers.map((item) => (
                   <ComboboxOption
                     key={item}
                     value={item}
-                    className={({ active}) => `realative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
-                  >{item}</ComboboxOption>
+                    className={({ active }) =>
+                      `realative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`
+                    }
+                    // {item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </ComboboxOption>
                 ))
               )}
             </ComboboxOptions>
